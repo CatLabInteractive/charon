2eae828a0f343fa22230e5e61ef40c377acded41
daedeloth <thijs@catlab.be>
2017-06-14 13:17:12 +0200

    Add helper method ResourceFieldCollection::getIncludedInContext to easily filter on context.

--- src/CatLab/Charon/Collections/ResourceFieldCollection.php
+++ src/CatLab/Charon/Collections/ResourceFieldCollection.php
@@ -3,6 +3,9 @@
 namespace CatLab\Charon\Collections;
 
 use CatLab\Base\Collections\Collection;
+use CatLab\Charon\Interfaces\Context;
+use CatLab\Charon\Models\CurrentPath;
+use CatLab\Charon\Models\Properties\Base\Field;
 use CatLab\Charon\Models\Properties\IdentifierField;
 use CatLab\Charon\Models\Properties\ResourceField;
 
@@ -47,6 +50,7 @@ class ResourceFieldCollection extends Collection
     {
         $out = new self();
         foreach ($this as $v) {
+            /** @var $v Field */
             if ($v->isSortable()) {
                 $out[] = $v;
             }
@@ -62,7 +66,31 @@ class ResourceFieldCollection extends Collection
     {
         $out = new self();
         foreach ($this as $v) {
+            /** @var $v Field */
             if ($v->isExpendable()) {
+                $out[] = $v;
+            }
+        }
+
+        return $out;
+    }
+
+    /**
+     * Get all resource fields that should be included in a given context
+     * @param Context $context
+     * @param CurrentPath|null $path
+     * @return array|ResourceFieldCollection
+     */
+    public function getIncludedInContext(Context $context, CurrentPath $path = null)
+    {
+        if (!isset($path)) {
+            $path = new CurrentPath();
+        }
+
+        $out = new self();
+        foreach ($this as $v) {
+            /** @var $v Field */
+            if ($v->shouldInclude($context, $path)) {
                 $out[] = $v;
             }
         }
