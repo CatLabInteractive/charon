00f2749a551e981305c7df51a253595605778dfb
Thijs Van der Schaeghe <thijs@catlab.be>
2018-02-26 22:54:04 +0100

    Make ParentEntityCollection Countable.

--- src/Collections/ParentEntityCollection.php
+++ src/Collections/ParentEntityCollection.php
@@ -6,7 +6,7 @@ namespace CatLab\Charon\Collections;
  * Class ParentEntities
  * @package CatLab\RESTResource\Collections
  */
-class ParentEntityCollection
+class ParentEntityCollection implements \Countable
 {
     private $entities = [];
 
@@ -29,5 +29,19 @@ class ParentEntityCollection
             return $this->entities[count($this->entities) - 2];
         }
         return null;
+    }
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return count($this->entities);
     }
 }
